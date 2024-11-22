8f3948e71cdcbd095b28d0f75a20cc5b3e7755cb
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-11-22 19:38:05 +0530

    Added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar';
 import MainRoutes from './routes/MainRoutes';
 
 function App() {
 
   return (
     <>
+      <Navbar/> 
       <MainRoutes/>
     </>
   )
